fe9e288561f8cbd1640ca8f43a9ca62b4d48b9b0
ych861031 <danny861031@gmail.com>
2018-01-01 01:20:48 +0800

    android commit

---
+++ app/src/androidTest/java/com/example/yangchunghsuan/myapplication/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.example.yangchunghsuan.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yangchunghsuan on 2018/1/1.
+ */
+public class MainActivityTest {
+
+    MainActivity a = new MainActivity();
+
+    @Test
+    public void t() throws Exception {
+        assertEquals(1,a.t(1));
+    }
+
+}

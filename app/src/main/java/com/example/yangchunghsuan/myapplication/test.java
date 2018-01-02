ff8d326194dfa1078d92a680e851957b2d297e54
ych861031 <danny861031@gmail.com>
2018-01-02 18:02:25 +0800

    commit coverege test2

---
+++ app/src/main/java/com/example/yangchunghsuan/myapplication/test.java
@@ -0,0 +1,12 @@
+package com.example.yangchunghsuan.myapplication;
+
+/**
+ * Created by yangchunghsuan on 2018/1/2.
+ */
+
+public class test {
+
+    public int t(int a){
+        return a;
+    }
+}
---
+++ app/src/test/java/com/example/yangchunghsuan/myapplication/testTest.java
@@ -0,0 +1,22 @@
+package com.example.yangchunghsuan.myapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yangchunghsuan on 2018/1/2.
+ */
+public class testTest {
+    @Before
+    public void setUp() throws Exception {
+        test a = new test();
+    }
+
+    @Test
+    public void t() throws Exception {
+        assertEquals(1,1);
+    }
+
+}
